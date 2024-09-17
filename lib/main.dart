6a28c9b9e8763982a772e45e225eb614a1357ef1
Amine-Triki <amine.triki15@gmail.com>
2024-09-17 16:38:09 +0100

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
               ),
                             SizedBox(
                 height: 10,
-              ),[]
+              ),
               Text(
                 'أمين التريكي',
                 style: TextStyle(
